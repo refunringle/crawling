f25d9c768171adf67c362654dce6d5d5cfb5c036
Refun <refunfk@gmail.com>
2022-08-20 21:03:18 +0530

    songs and lyrics are inserted into psql database

--- crawler.py
+++ crawler.py
@@ -4,11 +4,19 @@ import logging
 import requests
 from bs4 import BeautifulSoup 
 
+import db
+
 logger = None
 
 def parse_args():
     parser = argparse.ArgumentParser(description = "Web crawler")
     parser.add_argument("-d", "--debug", help = "Enable debug logging", action="store_true")
+    parser.add_argument("--db", help="Name of database to use", action="store", default="lyrics")
+    subcommands = parser.add_subparsers(help="Commands", dest="command", required=True)
+    subcommands.add_parser("initdb", help="Initialise the database")
+    subcommands.add_parser("crawl", help="Perform a crawl")
+    subcommands.add_parser("web", help="Start web server")
+    
     return parser.parse_args()
 
 def configure_logging(level=logging.INFO):
@@ -21,27 +29,27 @@ def configure_logging(level=logging.INFO):
     screen_handler.setFormatter(formatter)
     logger.addHandler(screen_handler)
 
-def artist(source):
+def artist(url_address):
     artist = dict()
-    response = requests.get(source)
+    response = requests.get(url_address)
     soup = BeautifulSoup(response.content, "lxml")
     track=soup.find('table', {'class':'tracklist'})
     headings =track.find_all('h3')
-    for heading in headings[:6]:
+    for heading in headings[5]:
         artist[heading.text] = heading.a['href']
         
     return artist
 
-def get_song_list(url):
-    song = dict()
+def get_songs_list(url):
+    songs = dict()
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "lxml")
     track=soup.find('table', {'class':'tracklist'})
     links =track.find_all('a')
-    for link in links[0:6]:
-        song[link.text] = link['href']
+    for link in links[5]:
+        songs[link.text] = link['href']
 
-    return song
+    return songs
 
 
 def song_lyrics(url):
@@ -50,24 +58,45 @@ def song_lyrics(url):
     paragraph=soup.find('p', {'id':'songLyricsDiv'})
     return paragraph.text
 
+def create_tables(db_name):
+    conn = db.get_connection(db_name)
+    with conn.cursor() as cursor:
+        with open("init.sql") as f:
+            sql = f.read()
+            cursor.execute(sql)
+    conn.commit()
+    conn.close()
+
+def insert_data_to_database(url):
+    for artist_name, artist_url in artist(url).items():
+        artist_id = db.add_artist(artist_name)
+        for songs, song_url in get_songs_list(artist_url).items():
+            lyrics = song_lyrics(song_url)
+            db.add_song(songs, artist_id, lyrics)
 
 
 def main():
     args = parse_args()
+    url_address= 'http://www.songlyrics.com/top-artists-lyrics.html'
+
     if args.debug:
         configure_logging(logging.DEBUG)
     else:
         configure_logging(logging.INFO)
+
+    if args.command == "crawl":
+        logger.info("Crawling")
+        artists = artist(url_address)
+        song_list = get_songs_list(list(artists.values())[0])
+        songs = song_lyrics(list(song_list.values())[0])
+        print(songs)
     
-    source= 'http://www.songlyrics.com/top-artists-lyrics.html'
-    artists = artist(source)
-#    print('meh', list(artists.values())[0])
-    song_list = get_song_list(list(artists.values())[0])
-    song = song_lyrics(list(song_list.values())[0])
-    print(song)
-#    song_lyric=artists
-    # print('meh', artist_play_list)
-  
+    elif args.command == "initdb":
+        logger.info("Initialising database")
+        create_tables(args.db)
+        insert_data_to_database(url_address)
+    else:
+        logger.warning("%s not implemented", args.command)
 
 if __name__ == "__main__":
     main()
--- db.py
+++ db.py
@@ -3,10 +3,23 @@ import psycopg2
 def get_connection(dbname="lyrics"):
     return psycopg2.connect(f"dbname={dbname}")
 
+
 def add_artist(artist_name):
     conn = get_connection()
     with conn.cursor() as curs:
         curs.execute("INSERT INTO artists(name) VALUES(%s)", (artist_name,))
+        curs.execute("SELECT id FROM artists ORDER BY id DESC")
+        id = curs.fetchone()[0]
     conn.commit()
     conn.close()
-    
+    return id
+
+def add_song(song_name, artist_id, lyrics):
+    conn = get_connection()
+    with conn.cursor() as curs:
+        curs.execute("INSERT INTO songs(name, artist_id, lyrics) VALUES(%s,%s, %s)",
+         (song_name, artist_id, lyrics))
+    conn.commit()
+    conn.close()
+
+
